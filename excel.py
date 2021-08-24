0040ddbf05501c4f7c09cf9c8d810a0825407432
hakase <cb634780751@qq.com>
2021-08-24 14:48:55 +0800

    Update excel.py

--- excel.py
+++ excel.py
@@ -6,7 +6,7 @@ class ExcelWriter:
     def __init__(self, name):
         self.workbook = xlwt.Workbook()
         self.name = name
-        self.sheet = self.workbook.add_sheet(self.name)
+        self.sheet = self.workbook.add_sheet('sheet')
 
     def put_data(self, arr):
         for idx, key in enumerate(arr[0]):
@@ -17,22 +17,20 @@ class ExcelWriter:
         self.workbook.save(self.name)
 
 
-class ExeclReader:
+class ExcelReader:
     def __init__(self, name):
         self.workbook = xlrd.open_workbook(name)
         self.name = name
         self.sheet = self.workbook.sheet_by_index(0)
-        self.col_num = self.sheet.ncols
-        self.row_num = self.sheet.nrows
 
     def get_data(self):
         arr, keys = [], []
-        for idx in range(self.col_num):
-            keys.append(self.sheet.cell(0, idx))
-        for idx in range(1, self.row_num):
+        for idx in range(self.sheet.ncols):
+            keys.append(self.sheet.cell(0, idx).value)
+        for idx in range(1, self.sheet.nrows):
             data = dict()
-            for jdx in range(self.col_num):
-                data[keys[jdx]] = self.sheet.cell(idx, jdx)
+            for jdx in range(self.sheet.ncols):
+                data[keys[jdx]] = self.sheet.cell(idx, jdx).value
             arr.append(data)
         return arr
 
