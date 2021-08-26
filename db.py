ad75eca4dd4a4ba37d5e8681ce0f5691d7a4639d
hakase <cb634780751@qq.com>
2021-08-26 22:19:37 +0800

    Create db.py

---
+++ db.py
@@ -0,0 +1,33 @@
+import MySQLdb
+
+
+class DB:
+    def __init__(self, host='localhost', user='root', passwd='123456', db='main', port=3306):
+        self.conn = MySQLdb.connect(host, user, passwd, db, port)
+        self.csr = self.conn.cursor()
+
+    def execute(self, sql):
+        try:
+            self.csr.execute(sql)
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+            self.conn.rollback()
+
+    def query(self, sql):
+        try:
+            self.csr.execute(sql)
+            data = self.csr.fetchall()
+            col_name = [cols[0] for cols in self.csr.description]
+            col_num = len(self.csr.description)
+            arr = []
+            for row in data:
+                kv = {}
+                for col in range(col_num):
+                    kv[col_name[col]] = row[col]
+                arr.append(kv)
+            return str(arr)
+        except Exception as e:
+            print(e)
+            return []
+
