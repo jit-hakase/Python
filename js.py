19bc8b35a5a9667bdf22260e6d5372b91908591a
hakase <cb634780751@qq.com>
2021-09-07 19:34:06 +0800

    Update js.py

--- js.py
+++ js.py
@@ -7,6 +7,8 @@ class JsonDumper(json.JSONEncoder):
             return obj.strftime('%Y-%m-%d %H-%M:%S')
         elif isinstance(obj, date):
             return obj.strftime('%Y-%m-%d')
+        elif isinstance(obj, bytes):
+            return str(obj)
         else:
             return json.JSONEncoder.default(self, obj)
 
