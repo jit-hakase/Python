814cc8442539ae3ca7d762bdcff4641ab0a8be4f
hakase <cb634780751@qq.com>
2021-09-14 15:56:27 +0800

    Update db.py

--- db.py
+++ db.py
@@ -3,14 +3,26 @@ import pymysql
 class DB:
     def __init__(self, _host='localhost', _user='root', _password='123456', _database='trade', _port=3306):
         self.conn = pymysql.connect(host=_host, user=_user, password=_password, database=_database, port=_port)
-        self.csr = self.conn.cursor()
+
+    def close(self):
+        self.conn.close()
+
+    def commit(self):
+        self.conn.commit()
+        self.csr.close()
+
+    def rollback(self):
+        self.conn.rollback()
+        self.csr.close()
 
     def execute(self, sql):
         self.conn.ping(reconnect=True)
+        self.csr = self.conn.cursor()
         self.csr.execute(sql)
 
     def query(self, sql):
         self.conn.ping(reconnect=True)
+        self.csr = self.conn.cursor()
         try:
             self.csr.execute(sql)
             data = self.csr.fetchall()
@@ -26,9 +38,3 @@ class DB:
         except Exception as e:
             print(e)
             return []
-        
-    def commit(self):
-        self.conn.commit()
-
-    def rollback(self):
-        self.conn.rollback()
