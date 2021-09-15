74b071f6a927c7fec6483bcfc1cb5e66dbfbaf2e
hakase <cb634780751@qq.com>
2021-09-15 16:26:06 +0800

    Update db.py

--- db.py
+++ db.py
@@ -3,29 +3,28 @@ import pymysql
 class DB:
     def __init__(self, _host='localhost', _user='root', _password='123456', _database='trade', _port=3306):
         self.conn = pymysql.connect(host=_host, user=_user, password=_password, database=_database, port=_port)
+        self.csr = self.conn.cursor()
 
     def close(self):
+        self.csr.close()
         self.conn.close()
 
     def commit(self):
         self.conn.commit()
-        self.csr.close()
 
     def rollback(self):
         self.conn.rollback()
-        self.csr.close()
 
     def execute(self, sql):
         self.conn.ping(reconnect=True)
-        self.csr = self.conn.cursor()
-        self.csr.execute(sql)
+        self.csr.close()
 
     def query(self, sql):
         self.conn.ping(reconnect=True)
-        self.csr = self.conn.cursor()
         try:
             self.csr.execute(sql)
             data = self.csr.fetchall()
+            self.commit()
             col_name = [cols[0] for cols in self.csr.description]
             col_num = len(self.csr.description)
             arr = []
