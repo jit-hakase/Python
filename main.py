014be6c46a08bfbc20243c5e3072bb4b19ac36ad
hakase <cb634780751@qq.com>
2021-09-17 14:23:35 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+from flask import Flask
+from multiprocessing import cpu_count, Process
+import datetime, os
+
+monkey.patch_all()
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return '200 Ok'
+
+def run(fork):
+    if not fork:
+        WSGIServer(('0.0.0.0', 8080), app).serve_forever()
+    else:
+        mulserver = WSGIServer(('0.0.0.0', 8080), app)
+        mulserver.start()
+
+        def serve_forever():
+            mulserver.start_accepting()
+            mulserver._stop_event.wait()
+
+        for i in range(cpu_count()):
+            p = Process(target=serve_forever)
+            p.start()
+
+if __name__ == '__main__':
+    run(fork=False)
