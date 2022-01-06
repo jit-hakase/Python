86193d0d3ecd6ef54a94da6938650fefc2a19917
hakase <cb634780751@qq.com>
2022-01-06 14:49:30 +0800

    Update db.py

--- util/db.py
+++ util/db.py
@@ -1,30 +1,39 @@
 import pymysql
+from queue import Queue
 
-class DB:
-    def __init__(self, _host='localhost', _user='root', _password='123456', _database='trade', _port=3306):
-        self.conn = pymysql.connect(host=_host, user=_user, password=_password, database=_database, port=_port)
+class SQLPool:
+    def __init__(self, num, _host='localhost', _user='root', _password='123456', _database='longx', _port=3306):
+        self.conns = Queue()
+        for i in range(num):
+            conn = pymysql.connect(host=_host, user=_user, password=_password, database=_database, port=_port)
+            self.conns.put(conn)
+
+    def acquire(self):
+        conn = self.conns.get()
+        conn.ping(reconnect=True)
+        return conn
+
+    def release(self, conn):
+        self.conns.put(conn)
+
+g_sql_pool = SQLPool(4)
+
+class SQLAdapter:
+    def __enter__(self):
+        self.conn = g_sql_pool.acquire()
         self.csr = self.conn.cursor()
+        return self
 
-    def close(self):
+    def __exit__(self, tp, val, trc):
         self.csr.close()
-        self.conn.close()
-
-    def commit(self):
-        self.conn.commit()
-
-    def rollback(self):
-        self.conn.rollback()
-
-    def execute(self, sql):
-        self.conn.ping(reconnect=True)
-        self.csr.execute(sql)
+        g_sql_pool.release(self.conn)
 
     def query(self, sql):
-        self.conn.ping(reconnect=True)
         try:
+            self.sql = sql
             self.csr.execute(sql)
             data = self.csr.fetchall()
-            self.commit()
+            self.conn.commit()
             col_name = [cols[0] for cols in self.csr.description]
             col_num = len(self.csr.description)
             arr = []
@@ -35,6 +44,14 @@ class DB:
                 arr.append(kv)
             return arr
         except Exception as e:
-            self.rollback()
-            print(e)
             return []
+
+    def execute(self, sql):
+        sql.sql = sql
+        self.csr.execute(sql)
+
+    def commit(self):
+        self.conn.commit()
+
+    def rollback():
+        self.conn.rollback()
